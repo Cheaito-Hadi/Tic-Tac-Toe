3c669e914f6a3656c455f2998efc0faaff050be4
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-07-14 01:28:02 +0300

    Adding round results

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const player1 = document.getElementById('player1')
 const player2 = document.getElementById('player2')
 const firstScore = document.getElementById('player1_score')
 const secondScore = document.getElementById('player2_score')
+const gameResult = document.getElementById('result')
+
+
 
 let wining_cases =
     [
@@ -20,67 +23,89 @@ let score1 = 0;
 let score2 = 0;
 
 
-for (let i = 1; i <= 9; i++) {
+for (let i = 1; i <= 9; i++)
+ {
     let cell = document.getElementById('cell' + i);
-    cell.addEventListener('click', function () {
+    cell.addEventListener('click', function ()
+     {
+        const name1 = player1.value;
+        const name2 = player2.value;
         let current_player = localStorage.getItem('player');
-        if (current_player == 'p1') {
-            cell.innerText = "x"
+        if (current_player == 'p1')
+         {
+            cell.innerText = "X"
             localStorage.setItem('player', 'p2');
             attemptsX.push(cell.id)
             if (attemptsX.length >= 3) {
                 for (let j = 0; j < wining_cases.length; j++) {
-                    if (matchArrays(wining_cases[j], attemptsX)) {
+                    if (matchArrays(wining_cases[j], attemptsX)) 
+                    {
                         game_won = true;
                         individualScore('p1')
+                        showResult(name1 + " Won!")
                         resetGame()
                         break;
                     }
                 }
             }
 
-        } else {
-            cell.innerText = "o"
+        }
+         else 
+        {  
+            cell.innerText = "O"
             localStorage.setItem('player', 'p1');
             attemptsO.push(cell.id)
-            if (attemptsO.length >= 3) {
-                for (let j = 0; j < wining_cases.length; j++) {
-                    if (matchArrays(wining_cases[j], attemptsO)) {
+            if (attemptsO.length >= 3)
+             {
+                for (let j = 0; j < wining_cases.length; j++)
+                 {
+                    if (matchArrays(wining_cases[j], attemptsO))
+                     {
                         game_won = true;
                         individualScore('p2')
+                        showResult(name2 + " Won!")
                         resetGame()
                         break;
                     }
                 }
             }
         }
-        if (attemptsO.length + attemptsX.length == 9 && game_won == false) {
-            console.log("Draw",)
+        if (attemptsO.length + attemptsX.length == 9 && game_won == false)
+         {
+            showResult("Draw-_-")
             resetGame()
         }
     });
 }
 
-function matchArrays(arr1, arr2) {
-    if (arr2.length < arr1.length) {
+function matchArrays(arr1, arr2)
+ {
+    if (arr2.length < arr1.length)
+     {
         return false
     }
     counter = 0
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.length; j++) {
-            if (arr1[i] == arr2[j]) {
+    for (let i = 0; i < arr1.length; i++)
+    {
+        for (let j = 0; j < arr2.length; j++) 
+        {
+            if (arr1[i] == arr2[j]) 
+            {
                 counter++
             }
         }
     }
-    if (counter == arr1.length) {
+    if (counter == arr1.length) 
+    {
         return true
     }
     return false
 }
 
-function resetGame() {
-    for (let i = 1; i <= 9; i++) {
+function resetGame() 
+{
+    for (let i = 1; i <= 9; i++)
+    {
         let cell = document.getElementById('cell' + i);
         cell.innerText = "";
     }
@@ -93,12 +118,14 @@ function resetGame() {
     addScore();
 }
 
-function addScore() {
+function addScore() 
+{
     firstScore.innerText = score1
     secondScore.innerText = score2
 }
 
-function individualScore(p) {
+function individualScore(p) 
+{
     if (p === 'p1') {
         score1++;
     }
@@ -108,11 +135,18 @@ function individualScore(p) {
     addScore()
 }
 
-function setName() {
+function setName() 
+{
     const name1 = player1.value;
     const name2 = player2.value;
     firstScore.innerText = name1;
     secondScore.innerText = name2;
+    
+}
+
+function showResult(string)
+{
+    gameResult.innerText = string
 }
 
 player2.addEventListener('change', setName)
